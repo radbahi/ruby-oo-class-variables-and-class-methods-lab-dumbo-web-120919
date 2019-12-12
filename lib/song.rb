91bdb3112f541ee060c6e1fbfe3f63b4997eb073
Radouane Bahi <radbahi@gmail.com>
2019-12-12 20:41:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,27 @@ end
 end
 
 def self.genre_count
-  
-end
+  genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+def self.artist_count
+artist_count = {}
+    @@artistss.each do |artist|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
   
 end #end of class Song
